411d04703a4b6c99bf3d3c6d86ed56fdfd26b290
Huzeyfe Canbaz <104665324+huzeyfecanbaz@users.noreply.github.com>
2022-08-28 13:38:25 -0600

    Add files via upload

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,33 +1,58 @@
 // add console.log to check to see if code is working
 console.log('working')
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
-
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//     }).addTo(map);
-
-L.circleMarker([34.0522, -118.2437],{
-    radius:300,
-    color:"black",
-    fillcolor:'#ffffa1'
-}).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
-//add a marker to the map
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//create a base layer that holds both maps
+let baseMaps={
+    Street:streets,
+    "Satellite Streets":satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods ="https://raw.githubusercontent.com/huzeyfecanbaz/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#0000FF",
+    weight: 8
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
 
