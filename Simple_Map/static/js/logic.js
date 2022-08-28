74a519bec2ec034b964c4e0ac28eeccc843162a4
Huzeyfe Canbaz <104665324+huzeyfecanbaz@users.noreply.github.com>
2022-08-28 11:54:12 -0600

    Add files via upload

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,7 +2,29 @@
 console.log('working')
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+// An array containing each city's location, state, and population.
+//get the data from cities.js
+let cityData=cities;
+
+
+// loop through the cities array and create one marker for each city
+// cityData.forEach(function(city){
+//     console.log(city)
+//     L.marker(city.location).bindPopup('<h2>'+city.city+", "+"</h2><hr><h3>Population "+city.population.toLocaleString()+"</h3>")
+//     .addTo(map);
+// })
+
+// loop through the cities array and create one circle for each city according to their population
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location,{
+        radius:city.population/100000
+    })
+    .bindPopup('<h2>'+city.city+", "+"</h2><hr><h3>Population "+city.population.toLocaleString()+"</h3>")
+    .addTo(map);
+})
 
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -11,11 +33,11 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //     radius: 100
 //     }).addTo(map);
 
-L.circleMarker([34.0522, -118.2437],{
-    radius:300,
-    color:"black",
-    fillcolor:'#ffffa1'
-}).addTo(map);
+// L.circleMarker([34.0522, -118.2437],{
+//     radius:300,
+//     color:"black",
+//     fillcolor:'#ffffa1'
+// }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
